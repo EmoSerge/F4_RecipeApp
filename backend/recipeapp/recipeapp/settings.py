9bbca042b975cae5d562d0caa295d20d851437dc
EmoSerge <vankin.s@mail.ru>
2023-07-11 18:35:25 +0300

    upd frontend

--- backend/recipeapp/recipeapp/settings.py
+++ backend/recipeapp/recipeapp/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d0*#zn=p2q$5rq^1zp)ybmx$n94)kin44x87suthzl!=a0gwmw'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
